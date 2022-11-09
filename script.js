4923d96d755575f99fb4f9e5ada71d5afbcf6b49
fouad ajayi <fouadajayi6@gmail.com>
2022-11-09 06:25:32 +0100

    Implemening funionalities

---
+++ script.js
@@ -0,0 +1,28 @@
+let  addform = document.querySelector('.add')
+let list = document.querySelector('.todos')
+
+
+let  generateTemplate = (todo) => {
+
+    let html = `
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span>${todo}</span>
+            <i class="far fa-trash-alt delete"></i>
+        </li>
+    `
+
+    list.innerHTML += html 
+}
+
+addform.addEventListener('click' , (e) => {
+
+    e.preventDefault();
+
+    let todo = addform.add.value.trim()
+    console.log(todo);
+
+    if(todo.length){
+        generateTemplate(todo)
+        addform.reset()
+    }
+})
